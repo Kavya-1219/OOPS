6327f3b61589ddab697de56bdc305a5c56db2609
Kavya-1219 <132338691+Kavya-1219@users.noreply.github.com>
2024-03-23 14:16:39 +0530

    Add files via upload

---
+++ use of datatypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    // Integer data type
+    int integerNumber = 10;
+
+    // Float data type
+    float floatNumber = 3.14;
+
+    // Character data type
+    char character = 'A';
+
+    // Output values
+    std::cout << "Integer Number: " << integerNumber << std::endl;
+    std::cout << "Float Number: " << floatNumber << std::endl;
+    std::cout << "Character: " << character << std::endl;
+
+    return 0;
+}
+
